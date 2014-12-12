4ac8be549931306667ec601c9f494a262a5c0e22
saucy <zekewarren@gmail.com>
2014-12-12 13:54:14 -0800

    Getting ready for working commit

--- index.js
+++ index.js
@@ -47,8 +47,84 @@ CProject.prototype = {
 
 };
 
-function activate(state) {
+var cprojects = {
 
+};
+
+function initCProject(projectRoot) {
+	console.log("initCProject: " + projectRoot.getPath());
+	var projectPath = projectRoot.getPath();
+	var projectName = projectRoot.getBaseName();
+	var els = document.querySelectorAll(".name[data-name=\""+projectName+"\"]");
+	var projectRootEl = null;
+
+	for(var elIndex in els) {
+		var el = els[elIndex];
+		if(el.getAttribute("data-path") == projectPath) {
+			projectRootEl = el;
+			break;
+		}
+	}
+
+	if(projectRootEl) {
+		projectRootEl.parentElement.classList.add("cproject-root");
+	}
+}
+
+function activate(state) {
+  try {
+    cprojects = JSON.parse(state);
+  } catch(e) { cprojects = {}; }
+  
+	console.log("CProject Activate");
+
+	var projectRootDirectory = null;
+	var disposeOld = function() {};
+
+	atom.contextMenu.add({
+		".cproject-root": [
+			{	label: "CProject",
+				submenu: [
+					{label: "Configure", command: "cproject:configure"},
+					{label: "Build", command: "cproject:build"},
+					{label: "Run", command: "cproject:run"}
+				]
+			}
+		]
+	});
+
+	Object.defineProperty(atom.project, "rootDirectory", {
+		get: function() {
+			return projectRootDirectory;
+		},
+		set: function(dir) {
+			disposeOld();
+			projectRootDirectory = dir;
+
+			function parseEntries() {
+				if(projectRootDirectory) {
+					projectRootDirectory.getEntries(function(error, entries) {
+						if(error) console.log(error);
+
+						for(var entryIndex in entries) {
+							var entry = entries[entryIndex];
+							if(entry.isFile()) {
+								if(entry.getBaseName() == "CMakeLists.txt") {
+									initCProject(projectRootDirectory);
+									break;
+								}
+							}
+						}
+					});
+				}
+			}
+
+			if(projectRootDirectory) {
+				parseEntries();
+				disposeOld = projectRootDirectory.onDidChange(parseEntries);
+			}
+		}
+	});
 }
 
 function deactivate() {
@@ -56,5 +132,5 @@ function deactivate() {
 }
 
 function serialize() {
-
+  return JSON.stringify(cprojects);
 }
